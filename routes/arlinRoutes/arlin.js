cb1878a46b0f5c34622bb473d5fe82c07f13cd60
Arlin Grijalba <arlin.grijalba@gmail.com>
2017-06-14 22:34:19 -0700

    Added comments to each type of route

--- routes/arlinRoutes/arlin.js
+++ routes/arlinRoutes/arlin.js
@@ -2,23 +2,28 @@
 const ex = require('express');
 const rutas = ex.Router();
 
+ //Simple route
 rutas.get('/', (req, res) => {
   res.send('Rutas de arlin');
 });
 
+//Prints something to the console
 rutas.get('/hola', (request, response) => {
   console.log('Dijeron hola');
   response.send('Di hola!');
 });
 
+//body of request (url)
 rutas.get('/test', function(req, res){
   res.send(req.body);
 });
 
+//responds with a json object
 rutas.get('/json', (req, res)=> {
   res.json({name:'Arlin', age:25, cool:true});
 });
 
+//responds with the params received from the url
 rutas.get('/req', (req, res) => {
   var response = 'Echoing req params: <br>';
   var name = 'Name: ' + req.query.name + '<br>';
@@ -26,10 +31,13 @@ rutas.get('/req', (req, res) => {
   res.send(response + name + age);
 });
 
+//responds with the query object of the response
+//that has all the params of the url query
 rutas.get('/req/all', (req, res) => {
   res.send(req.query);
 });
 
+//access to the variable of the url
 rutas.get('/sayHi/:name', (req, res) => {
   res.send('HOLA ' + req.params.name);
 });
