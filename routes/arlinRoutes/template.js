836ceb3081c82337af404712daa9aee3e4a87c5e
Arlin Grijalba <arlin.grijalba@gmail.com>
2017-06-15 12:24:21 -0700

    Pass variables from url to template view

--- routes/arlinRoutes/template.js
+++ routes/arlinRoutes/template.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
+// router.get('/', (req, res) => {
+//   res.render('hello');
+// });
+
+// pass variables of the query url to view template
 router.get('/', (req, res) => {
-  res.render('hello');
+  res.render('hello', {
+    dog: req.query.dog
+  });
 });
 
 module.exports = router;
