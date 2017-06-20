772acdc9ee40c4f0e4b112b31e93311e110c9969
Arlin Grijalba <arlin.grijalba@gmail.com>
2017-06-19 20:48:01 -0700

    Created a model for the Stores

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,15 +1,16 @@
-exports.myMiddleware = (req, res, next) => {
-  req.name= 'Arlin';
-  next();
-};
+// exports.myMiddleware = (req, res, next) => {
+//   req.name= 'Arlin';
+//   next();
+// };
 
 exports.homePage = (req, res) => {
   res.render('index', {
-    title: 'Middleware',
-    name: req.name
+    title: 'Middleware'
+    // name: req.name
   });
 };
 
+//delete later
 exports.testMiddleware = (req, res, next) =>{
   next();
 };
