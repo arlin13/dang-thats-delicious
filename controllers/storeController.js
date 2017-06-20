d46ab75d5019b88e7e7aaa8770d992a6bdd9aff9
Arlin Grijalba <arlin.grijalba@gmail.com>
2017-06-19 20:07:11 -0700

    Created two middleware (locals)

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,3 +1,22 @@
-exports.homePage = (req, res) => {
-  res.render('index');
+exports.myMiddleware = (req, res, next) => {
+  req.name= 'Arlin';
+  next();
 };
+
+exports.homePage = (req, res) => {
+  res.render('index', {
+    title: 'Middleware',
+    name: req.name
+  });
+};
+
+exports.testMiddleware = (req, res, next) =>{
+  next();
+};
+
+exports.test = function(req, res) {
+  res.render('index', {
+    title: 'myMiddleware test',
+    name: 'my myMiddleware test'
+  });
+}
